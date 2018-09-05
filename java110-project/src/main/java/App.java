1a76a41e2b00216eca1a983ff0bd1d8c8551764c
yeo <gpfla757@naver.com>
2018-09-05 10:17:52 +0900

    메뉴기능 추가

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
  */
 public class App {
 
-    
+
     //여러 속성의 값을 관리하기 쉽도록 사용자 정의 데이터 타입을 만들어 사용한다.
     static class Member{
         protected String name;
         protected String email;
         protected String password;
-        
+
         //인스턴스의 메모리를 다루는 operator=setter/getter=accessor=propterty=message
         public String getName() {
             return name;
@@ -31,24 +31,98 @@ public class App {
         public void setPassword(String password) {
             this.password = password;
         }
-        
-        
+
+
     }
 
-    static Member[] members = new Member[100];
+    static class Student extends Member{
+        protected String school;
+        protected boolean working;
+        protected String tel;
+        
+        public String getSchool() {
+            return school;
+        }
+        public void setSchool(String school) {
+            this.school = school;
+        }
+        public boolean isWorking() {
+            return working;
+        }
+        public void setWorking(boolean working) {
+            this.working = working;
+        }
+        public String getTel() {
+            return tel;
+        }
+        public void setTel(String tel) {
+            this.tel = tel;
+        }
+    }
+    
+    static Student[] students = new Student[100];
 
     static int index = 0;
 
     static Scanner keyIn = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
 
-        inputMembers();
-        
-        printMembers();
+        while(true) {
+            String menu = promptMenu();
+
+            if(menu.equals("1")) {
+                serviceStudentMenu();
+            }else if(menu.equals("0")){
+                System.out.println("안녕히가세요!");
+                break;
+            }
+        }
         
         keyIn.close();
 
+    }
+
+    private static void serviceStudentMenu() {
+
+        while(true) {
+            System.out.print("학생관리> ");
+            String command = keyIn.nextLine();
+            if(command.equals("list")) {
+                printMembers();
+            }else if(command.equals("add")){
+                inputMembers();
+            }else if(command.equals("quit")){
+                break;
+            }else {
+                System.out.println("유효하지 않는 명령입니다.");
+            }
+        }
+    }
+
+    private static String promptMenu() {
+        System.out.println("[메뉴]");
+        System.out.println("1. 학생관리");
+        System.out.println("2. 강사관리");
+        System.out.println("3. 매니저관리");
+        System.out.println("0. 종료");
+
+        while(true) {
+            System.out.print("메뉴 번호> ");
+
+            String menu = keyIn.nextLine();
+            System.out.println(menu);
+
+            switch(menu) {
+            case "1":
+            case "2":
+            case "3":
+            case "0":
+                return menu;
+            default:
+                System.out.println("메뉴 번호가 유효하지 않습니다.");
+            }
+        }
     }
 
     static void printMembers() {
@@ -62,8 +136,8 @@ public class App {
 
     static void inputMembers() {
         while(true) {
-           Member m = new Member();
-            
+            Member m = new Member();
+
             System.out.print("이름? ");
             m.setName(keyIn.nextLine());
 
@@ -74,7 +148,7 @@ public class App {
             m.setPassword(keyIn.nextLine());
 
             members[index++] = m;
-            
+
             System.out.println("계속 하시겠습니까?(Y/n) ");
             String answer = keyIn.nextLine();
             if(answer.toLowerCase().equals("n")) {
