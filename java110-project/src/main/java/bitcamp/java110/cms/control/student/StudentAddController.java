c812e6beec2a1c9120dea71d0e0d356c8924eb68
yeo <gpfla757@naver.com>
2018-09-10 15:49:12 +0900

    학생관리에 DAO적용

--- java110-project/src/main/java/bitcamp/java110/cms/control/student/StudentAddController.java
+++ java110-project/src/main/java/bitcamp/java110/cms/control/student/StudentAddController.java
@@ -9,59 +9,67 @@ import bitcamp.java110.cms.domain.Student;
 
 @Component
 public class StudentAddController {
-    
+
     @RequestMapping("student/add")
     public void add(Scanner keyIn) {
         while (true) {
             Student m = new Student();
-            
+
             System.out.print("이름? ");
             m.setName(keyIn.nextLine());
-            
+
             System.out.print("이메일? ");
             m.setEmail(keyIn.nextLine());
-            
+
             System.out.print("암호? ");
             m.setPassword(keyIn.nextLine());
-            
+
             System.out.print("최종학력? ");
             m.setSchool(keyIn.nextLine());
-            
+
             System.out.print("재직여부?(true/false) ");
             m.setWorking(Boolean.parseBoolean(keyIn.nextLine()));
-            
+
             System.out.print("전화? ");
             m.setTel(keyIn.nextLine());
-            
-            App.students.add(m);
-            
+
+            if(App.studentDao.insert(m)>0) {
+                System.out.println("저장하였습니다.");
+            }else {
+                System.out.println("같은 이메일의 학생이 존재합니다.");
+            }
+
             System.out.print("계속 하시겠습니까?(Y/n) ");
             String answer = keyIn.nextLine();
             if (answer.toLowerCase().equals("n"))
                 break;
         }
     }
-    
-    
+
     {
         Student s = new Student();
         s.setName("a");
-        App.students.add(s);
-        
+        s.setEmail("a@test.com");
+        App.studentDao.insert(s);
+
         s = new Student();
         s.setName("b");
-        App.students.add(s);
-        
+        s.setEmail("b@test.com");
+        App.studentDao.insert(s);
+
         s = new Student();
         s.setName("c");
-        App.students.add(s);
-        
+        s.setEmail("c@test.com");
+        App.studentDao.insert(s);
+
         s = new Student();
         s.setName("d");
-        App.students.add(s);
-        
+        s.setEmail("d@test.com");
+        App.studentDao.insert(s);
+
         s = new Student();
         s.setName("e");
-        App.students.add(s);
+        s.setEmail("e@test.com");
+        App.studentDao.insert(s);
     }
 }
