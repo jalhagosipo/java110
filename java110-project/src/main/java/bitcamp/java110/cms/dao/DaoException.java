ab2de3857aa4a8e1b313543077a4d0d5b378765f
yeo <gpfla757@naver.com>
2018-09-13 15:49:54 +0900

    JDBC를 이용하여 매니저 목록 가져오기

--- java110-project/src/main/java/bitcamp/java110/cms/dao/DaoException.java
+++ java110-project/src/main/java/bitcamp/java110/cms/dao/DaoException.java
@@ -1,6 +1,6 @@
 package bitcamp.java110.cms.dao;
 
-public class DaoException extends Exception{
+public class DaoException extends RuntimeException{
 
     private static final long serialVersionUID = 1L;
 
@@ -16,5 +16,10 @@ public class DaoException extends Exception{
     public DaoException(String message) {
         super(message);
     }
-
+    public DaoException(Throwable cause) {
+        super(cause);
+    }
+    public DaoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
