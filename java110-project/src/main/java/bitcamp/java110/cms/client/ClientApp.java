63431c53ea91bc14ca691bcfbf61d7b436e1a738
yeo <gpfla757@naver.com>
2018-09-14 17:06:29 +0900

    xxxx/list 요청 처리

--- java110-project/src/main/java/bitcamp/java110/cms/client/ClientApp.java
+++ java110-project/src/main/java/bitcamp/java110/cms/client/ClientApp.java
@@ -16,7 +16,7 @@ public class ClientApp {
         try(//autocloseable을 구현한애들만가능.
             
             // 서버에 연결하기
-            Socket socket = new Socket("localhost",8888);
+            Socket socket = new Socket("192.168.0.10",8888);
             
             // 서버에 데이터를 보내고 읽을 도구를 준비한다.
             PrintStream out = new PrintStream(
