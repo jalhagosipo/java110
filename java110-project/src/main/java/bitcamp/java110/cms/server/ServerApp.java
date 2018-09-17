a7dd2eae5917d1f4e3654379f8a0c45617b0273b
yeo <gpfla757@naver.com>
2018-09-17 17:28:36 +0900

    tcp(Stateless) 적용

--- java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp.java
+++ java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp.java
@@ -50,11 +50,12 @@ public class ServerApp {
 
         // 클라이언트 연결을 기다리는 서버 소켓 준비
         ServerSocket serverSocket = new ServerSocket(8888);
-        
+
         System.out.println("서버 실행 중...");
 
         while(true) {
             Socket socket = serverSocket.accept();
+            System.out.println("클라이언트가 연결되었음!");
             RequestWorker worker = new RequestWorker(socket);
             /*Thread t = new Thread(worker);
             t.start();*/
@@ -66,18 +67,18 @@ public class ServerApp {
         ServerApp serverApp = new ServerApp();
         serverApp.service();
     }
-    
+
     class RequestWorker implements Runnable{
 
         Socket socket;
-        
+
         public RequestWorker(Socket socket) {
             this.socket = socket;
         }
-        
+
         @Override
         public void run() {
-            
+
             // 이 메서드에 "main" 스레드에서 분리하여 독립적으로 수행할 코드를 둔다.
             try(
                     Socket socket = this.socket; // 이렇게 로컬변수로 따로만든이유는 try()에 값이 있어야지만 try를 벗어날때 자동으로 close를 호출하기때문임. 
@@ -87,47 +88,41 @@ public class ServerApp {
                     BufferedReader in = new BufferedReader(
                             new InputStreamReader(
                                     socket.getInputStream())); 
-            ){
-                System.out.println(in.readLine());
-                out.println("OK:요");out.flush();
+                    ){
+                
+                String requestLine = in.readLine();
+                System.out.println("클라이언트 요청 받았음!");
+                
+                // 요청 객체 준비
+                Request request = new Request(requestLine);
 
-                while(true) {
-                    String requestLine = in.readLine();
-                    if(requestLine.equals("EXIT")) {
-                        out.println("goodbye");
-                        out.println();
-                        out.flush();
-                        break;
-                    }
-                    
-                    // 요청 객체 준비
-                    Request request = new Request(requestLine);
-                    
-                    // 응답 객체 준비
-                    Response response = new Response(out);
-                    
-                    RequestMappingHandler mapping = 
-                            requestHandlerMap.getMapping(request.getAppPath());
-                    if (mapping == null) {
-                        out.println("해당 요청을 처리할 수 없습니다.");
-                        out.println();
-                        out.flush();
-                        continue;
-                    }
-                    
-                    try {
-                        // 요청 핸들러 호출
-                        mapping.getMethod().invoke(mapping.getInstance(), request,response); // static메소드를 호출할땐 첫번재매개변수에 null을주면됨
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        out.println("요청 처리 중에 오류가 발생했습니다.");
-                    }
+                // 응답 객체 준비
+                Response response = new Response(out);
+
+                RequestMappingHandler mapping = 
+                        requestHandlerMap.getMapping(request.getAppPath());
+                if (mapping == null) {
+                    out.println("해당 요청을 처리할 수 없습니다.");
                     out.println();
                     out.flush();
+                    return;
                 }
+
+                try {
+                    // 요청 핸들러 호출
+                    mapping.getMethod().invoke(mapping.getInstance(), request,response); // static메소드를 호출할땐 첫번재매개변수에 null을주면됨
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    out.println("요청 처리 중에 오류가 발생했습니다.");
+                }
+                out.println();
+                out.flush();
                 
             }catch(Exception e) {
                 System.out.println(e.getMessage());
+            }finally {
+                System.out.println("클라이언트에게 응답했음!");
+                System.out.println("클라이언트와 연결을 끊음!");
             }
         }// run()
     }// RequestWorker class
