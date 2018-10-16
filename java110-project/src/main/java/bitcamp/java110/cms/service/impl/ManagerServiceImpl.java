8dfd76c11083662a3922780795f05ce275405dec
yeo <gpfla757@naver.com>
2018-10-16 16:41:30 +0900

    mybatis를 이용하여 DAO 구현체 제거하기, 트랜잭션 적용

--- java110-project/src/main/java/bitcamp/java110/cms/service/impl/ManagerServiceImpl.java
+++ java110-project/src/main/java/bitcamp/java110/cms/service/impl/ManagerServiceImpl.java
@@ -3,6 +3,9 @@ package bitcamp.java110.cms.service.impl;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
 import bitcamp.java110.cms.dao.ManagerDao;
 import bitcamp.java110.cms.dao.MemberDao;
 import bitcamp.java110.cms.dao.PhotoDao;
@@ -11,60 +14,93 @@ import bitcamp.java110.cms.service.ManagerService;
 
 public class ManagerServiceImpl implements ManagerService{
 
-    MemberDao memberDao;
-    ManagerDao managerDao;
-    PhotoDao photoDao;
+    SqlSessionFactory sqlSessionFactory;
 
-    public void setMemberDao(MemberDao memberDao) {
-        this.memberDao = memberDao;
-    }
-
-    public void setManagerDao(ManagerDao managerDao) {
-        this.managerDao = managerDao;
-    }
-
-    public void setPhotoDao(PhotoDao photoDao) {
-        this.photoDao = photoDao;
+    public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+        this.sqlSessionFactory = sqlSessionFactory;
     }
 
     @Override
     public void add(Manager manager) {
 
-        memberDao.insert(manager);
-        managerDao.insert(manager);
+        SqlSession session = sqlSessionFactory.openSession();// openSession괄호안default가 false임
 
-        if(manager.getPhoto() != null) {
-            HashMap<String,Object> params = new HashMap<>();
-            params.put("no",manager.getNo());
-            params.put("photo",manager.getPhoto());
+        try{
+            MemberDao memberDao = session.getMapper(MemberDao.class);
+            ManagerDao managerDao = session.getMapper(ManagerDao.class);
+            PhotoDao photoDao = session.getMapper(PhotoDao.class);
 
-            photoDao.insert(params);
+            memberDao.insert(manager);
+            managerDao.insert(manager);
+
+            if(manager.getPhoto() != null) {
+                HashMap<String,Object> params = new HashMap<>();
+                params.put("no",manager.getNo());
+                params.put("photo",manager.getPhoto());
+
+                photoDao.insert(params);
+            }
+
+            session.commit();
+            
+        }catch(Exception e) {
+            session.rollback();
+            throw e;
+        }finally {
+            session.close();
         }
     }
 
     @Override
     public List<Manager> list(int pageNo,int pageSize) {
 
-        HashMap<String,Object> params = new HashMap<>();
-        params.put("rowNo",(pageNo-1)*pageSize);
-        params.put("size",pageSize);
+        try(SqlSession session = sqlSessionFactory.openSession()){
+            
+            ManagerDao managerDao = session.getMapper(ManagerDao.class);
+            
+            HashMap<String,Object> params = new HashMap<>();
+            params.put("rowNo",(pageNo-1)*pageSize);
+            params.put("size",pageSize);
 
-        return managerDao.findAll(params);
+            return managerDao.findAll(params);
+        }
     }
 
     @Override
     public Manager get(int no) {
-        return managerDao.findByNo(no);
+
+        try(SqlSession session = sqlSessionFactory.openSession()){
+            
+            ManagerDao managerDao = session.getMapper(ManagerDao.class);
+            
+            return managerDao.findByNo(no);
+        }
     }
 
     @Override
     public void delete(int no) {
 
-        if(managerDao.delete(no) == 0) {
-            throw new RuntimeException("해당 번호의 데이터가 없습니다.");
-        }
-        photoDao.delete(no);
-        memberDao.delete(no);
+        SqlSession session = sqlSessionFactory.openSession();
+        
+        try{
+            
+            MemberDao memberDao = session.getMapper(MemberDao.class);
+            ManagerDao managerDao = session.getMapper(ManagerDao.class);
+            PhotoDao photoDao = session.getMapper(PhotoDao.class);
+            
+            if(managerDao.delete(no) == 0) {
+                throw new RuntimeException("해당 번호의 데이터가 없습니다.");
+            }
+            photoDao.delete(no);
+            memberDao.delete(no);
 
+            session.commit();
+            
+        }catch(Exception e) {
+            session.rollback();
+            throw e;
+        }finally {
+            session.close();
+        }
     }
 }
